f47f1493f0e7de6005cf22d6ab76b52a1b7325bc
Willian Santos <m16@m16.com.br>
2023-04-09 13:34:55 -0300

    Exemplo C# 6

--- VSC7HelloWorld/Program.cs
+++ VSC7HelloWorld/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace VSC7HelloWorld
+{
+class Program
+    {
+        static void Main(string[] args)
+        {
+            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
